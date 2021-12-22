f689d74b4e70926668075b462b960c9a2f18b491
Ajesh01 <ajeshmartin01@gmail.com>
2021-12-23 00:02:14 +0530

    text classification base code

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,30 @@
 from flask import Flask
+import os
+import json
+
+
+
+import spacy
+from spacy.language import Language
+from spacy_langdetect import LanguageDetector
+
+
+
+
 
 app = Flask(__name__)
 
+
+tweets_list2 = []
+
+
+def get_lang_detector(nlp, name):
+    return LanguageDetector()
+
+
+nlp = spacy.load("en_core_web_sm")
+Language.factory("language_detector", func=get_lang_detector)
+nlp.add_pipe('language_detector', last=True)
 
 
 
@@ -10,5 +33,58 @@ app = Flask(__name__)
 def test():
 
     return { "result" : "testing OK" }
+
+
+
+
+
+
+@app.route('/latest-tweets', methods = ["GET"]) 
+def get_latest_tweets():
+
+    # gets tweets and saves it in a jsonl format
+    os.system("snscrape --jsonl --max-results 50 --since 2020-06-01 twitter-search 'flood' > text-query-tweets.json")
+    # print(count(tweets_list2)) 
+ 
+    tweets = []
+    for line in open('text-query-tweets.json', 'r'):
+        tweet = json.loads(line)
+        # isReliable, textBytesFound, details = cld2.detect("а неправильный формат идентификатора дн назад")
+        # print(details)
+        text_content = tweet["content"]
+        doc = nlp(text_content) 
+        detect_language = doc._.language
+        english_flag = False
+
+        #  filters tweets in english
+        if detect_language["language"] == 'en' and detect_language['score'] >= 0.85:
+            english_flag = True
+            tweet["is_english"] = english_flag
+            tweets.append(tweet)
+
+
+        
+    # pprint.pprint(tweets[0])
+
+
+
+
+    print(detect_language)
+
+    return { "result" : "success" }
+
+
+import en_textcat_demo
+
+nlp = en_textcat_demo.load()
+text = ['yea right, so funny', 'You forgot "Am I a joke to you" guy', "Pretty ballsey for Ted to mouth off to his bosses like that.", "I’m sure she is going to make an absolutely stellar cop, with no power trip agenda whatsoever", "Shut up, college boy!!"]
+
+docs = list(nlp.pipe(text))
+result = []
+for doc in docs:
+    print(doc.text)
+    print(f"{doc.cats}\n")
+
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0",port=5555)
--- web-ui/app.py
+++ web-ui/app.py
@@ -1,9 +1,10 @@
+from itertools import count
 from flask import Flask, render_template
 import requests
 
+
+
 app = Flask(__name__)
-
-
 
 
 
@@ -12,13 +13,18 @@ app = Flask(__name__)
 @app.route('/', methods = ["GET"]) 
 def index():
 
-    URL = "http://172.20.0.2:5555/test"
+    URL = "http://172.20.0.2:5555/latest-tweets"
     PARAMS = {'address':'hey'}
     r = requests.get(url = URL)
     data = r.json()
     return str(data)
-    # print(r.data)
-    return render_template("index.html")
+    print(r.data)
+
+
+
+    
+
+    # return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0",port=8080)
